929918b50560d845079c1ce3dc389404784b3ffb
Plutvik <rysavy.jir@gmail.com>
2025-07-10 15:32:26 +0200

    Basic structure with automatically showing photos from the images folder

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
     <title>Tondův Chrám</title>
 </head>
 <body>
-
+    <div class="container">
+        <h1>Tondův Chrám</h1>
+        <div class="gallery">
+            <?php
+                $images = glob("images/*.{jpg,jpeg,png,gif}", GLOB_BRACE);
+                foreach($images as $image) {
+                    echo "<a href='$image'><img src='$image' alt='image'></a>";
+                }
+            ?>
+        </div>
+    </div>
 </body>
 </html>
